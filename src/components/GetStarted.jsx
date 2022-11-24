a4a7824520b21b21c5fa72c050ff0df37dc2fbfa
Ismail Tiamiyu <tiami.abiola@gmail.com>
2022-11-24 22:48:11 +0100

    uploading my new bankApp project

---
+++ src/components/GetStarted.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import styles from "../styles";
+import { arrowUp } from "../assets";
+
+const GetStarted = () => <div className={`${styles.flexCenter}`}></div>;
+
+export default GetStarted;
---
+++ src/components/index.js
@@ -0,0 +1,29 @@
+import Billing from "./Billing";
+import Navbar from "./Navbar";
+import Button from "./Button";
+import Cta from "./Cta";
+import Business from "./Business";
+import Stats from "./Stats";
+import Footer from "./Footer";
+import Clients from "./Clients";
+import FeedbackCard from "./FeedbackCard";
+import GetStarted from "./GetStarted";
+import CardDeal from "./CardDeal";
+import Hero from "./Hero";
+import Testimonials from "./Testimonials";
+
+export {
+	Stats,
+	Business,
+	Cta,
+	Billing,
+	CardDeal,
+	Testimonials,
+	Clients,
+	Hero,
+	Button,
+	Navbar,
+	FeedbackCard,
+	GetStarted,
+	Footer,
+};
