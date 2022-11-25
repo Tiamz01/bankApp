ce34777bdaa7ce398e5352c72898dad41fe8346f
Ismail Tiamiyu <tiami.abiola@gmail.com>
2022-11-25 20:09:58 +0100

    updating..

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -24,16 +24,41 @@ const Hero = () => (
 			</div>
 
 			<div className='flex flex-row justify-between items-center w-full'>
-				<h1 className='flex-1 font-poppins font-semibold text-white ss:text-[72px] ss:leading-[100px] leading-[75px] '>
+				<h1 className='flex-1 font-poppins font-semibold text-white text-[52px] ss:text-[72px] ss:leading-[100px] leading-[75px] '>
 					The Next <br className='sm:block hidden' />{" "}
 					<span className='text-gradient'> Generation </span>
-					<br className='sm:block hidden' /> Payment Method.
+					<br className='sm:block hidden' />
 				</h1>
 
 				<div className='ss:flex hidden md:mr-4 mr-0'>
 					<GetStarted />
 				</div>
 			</div>
+
+			<h1 className='flex-1 font-poppins  block font-semibold text-white ss:text-[68px] text-[32px] ss:leading-[100px] leading-[75px] w-full '>
+				Payment Method.
+			</h1>
+			<p className={`${styles.paragraph} max-w-[470px] mt-5 text-justify`}>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, odio,
+				necessitatibus ducimus omnis commodi quia enim nulla quibusdam placeat
+				officiis dolorum dolorem perferendis repellat temporibus ex impedit
+				nihil dignissimos cumque excepturi.
+			</p>
+		</div>
+		<div className={`${styles.flexCenter}flex-1 flex-row relative`}>
+			<img
+				src={robot}
+				alt='robot'
+				className='w-[100%]h-[100%}  relative z-[5]'
+			/>
+			<div className='absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient'></div>
+			<div className='absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient'></div>
+
+			<div className='absolute z-[1] w-[50%] h-[50%] bottom-20 right-20 rounded-full blue__gradient'></div>
+		</div>
+
+		<div className={`ss:hidden ${styles.flexCenter}`}>
+			<GetStarted />
 		</div>
 	</section>
 );
